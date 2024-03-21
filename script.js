faf089d49d0a3ca63181709e5b501d94cf5a6c1e
overtonjust <joverton@pursuit.org>
2024-03-21 12:37:36 -0400

    project v1 complete

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const list = document.querySelector('.list');
 const form = document.querySelectorAll('.field');
+const defaultInputs = document.querySelectorAll('.placeholders')
 const submit = form[5];
+const errorMsg = document.querySelector('.form__error');
+
 
 
 submit.addEventListener('click', () => {
+    
+    
     const book = document.createElement('article')
     book.setAttribute('class', 'book');
 
         const image = document.createElement('img');
         image.setAttribute('class', 'book__img');
-        image.setAttribute('src', form[2].value);
+        if(form[2].value == ''){
+            image.setAttribute('src', 'https://images.pexels.com/photos/3747279/pexels-photo-3747279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+        }
+
+        else {
+            image.setAttribute('src', form[2].value);
+        }
+
         image.setAttribute('alt', `${form[0].value} book cover`);
 
         const content = document.createElement('div');
@@ -18,10 +30,30 @@ submit.addEventListener('click', () => {
             const title = document.createElement('p');
             title.setAttribute('class','book__title' );
             title.textContent = form[0].value;
+            if(!form[0].checkValidity()) {
+                form[0].setAttribute('id', 'invalid');
+                errorMsg.textContent = `Please enter the book's title.`;
+                return;
+            }
+            else {
+                if(form[0].hasAttribute('id')) {
+                    form[0].removeAttribute('id');
+                }
+            }
 
             const author = document.createElement('p');
             author.setAttribute('class', 'book__author')
             author.textContent = form[1].value;
+            if(!form[1].checkValidity()){
+                form[1].setAttribute('id', 'invalid')
+                errorMsg.textContent = 'Please give the books author.'
+                return;
+            }
+            else {
+                if(form[1].hasAttribute('id')) {
+                    form[1].removeAttribute('id');
+                }
+            }
 
             const bubbleHolder = document.createElement('p')
                 const bubble = document.createElement('span');
@@ -40,6 +72,16 @@ submit.addEventListener('click', () => {
             const price = document.createElement('p');
             price.setAttribute('class', 'book__price');
             price.textContent = `$${form[3].value}`;
+            if(!form[3].checkValidity()) {
+                form[3].setAttribute('id', 'invalid')
+                errorMsg.innerHTML = 'Please enter book cost <i>(in 00.00 format)</i>.';
+                return;
+            }
+            else {
+                if(form[3].hasAttribute('id')) {
+                    form[3].removeAttribute('id');
+                }
+            }
 
         content.append(title, author, bubbleHolder, price);
 
@@ -52,6 +94,10 @@ submit.addEventListener('click', () => {
     book.append(image, content, icon);
 
     list.appendChild(book);
+
+    defaultInputs.forEach(input => {
+        input.value = '';
+    })
 });
 
 
